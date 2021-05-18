6a38d97146faf836e36ea0ac65da6bb8dbbd8cab
zeinebkefi <zeinebkefi@insat.u-carthage.tn>
2021-05-18 23:07:46 +0100

    shops

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -11,7 +11,7 @@ class ShopController extends AbstractController
     #[Route('/shop', name: 'shop')]
     public function index(): Response
     {
-        return $this->render('shop/index.html.twig', [
+        return $this->render('shop/shop.html.twig', [
             'controller_name' => 'ShopController',
         ]);
     }
---
+++ src/Entity/Product.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $animal;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getAnimal(): ?string
+    {
+        return $this->animal;
+    }
+
+    public function setAnimal(string $animal): self
+    {
+        $this->animal = $animal;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
