2b0d9fb37b52f9a43e9d0f987cd98822e89a99ca
moataz-hellal <moatazhellal@insat.u-carthage.tn>
2021-06-12 18:46:52 +0100

    register form + login page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $manager,Request $request): Response
     {
+        $user=new User();
+        $form=$this->createForm(UserType::class,$user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $manager->persist($user);
+            $manager->flush();
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'user'=>$user,
+            'form'=>$form->createView()
         ]);
     }
+    /*public function addUser(EntityManagerInterface $manager)
+    {
+        $user=new User();
+        $form=$this->createForm(UserType::class,$user);
+        return $this->render('home/index.html.twig',[
+            'user'=>$user,
+            'form'=>$form->createView()
+        ]);
+    }*/
 }
