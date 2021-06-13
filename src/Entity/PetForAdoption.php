2e164400ea68f9befc9ed2e13b2dee54cc57befb
yahyaouisalma82 <yahyaouisalma14@gmail.com>
2021-06-13 17:25:27 +0100

    adding wishlist

--- src/Entity/PetForAdoption.php
+++ src/Entity/PetForAdoption.php
@@ -59,20 +59,6 @@ class PetForAdoption
      */
     private $photoFile;
 
-    /**
-     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="myPetsForAdoption")
-     */
-    private $owner;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="pet")
-     */
-    private $comments;
-
-    public function __construct()
-    {
-        $this->comments = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -175,45 +161,6 @@ class PetForAdoption
         return $this;
     }
 
-    public function getOwner(): ?User
-    {
-        return $this->owner;
-    }
 
-    public function setOwner(?User $owner): self
-    {
-        $this->owner = $owner;
 
-        return $this;
-    }
-
-    /**
-     * @return Collection|Comment[]
-     */
-    public function getComments(): Collection
-    {
-        return $this->comments;
-    }
-
-    public function addComment(Comment $comment): self
-    {
-        if (!$this->comments->contains($comment)) {
-            $this->comments[] = $comment;
-            $comment->setPet($this);
-        }
-
-        return $this;
-    }
-
-    public function removeComment(Comment $comment): self
-    {
-        if ($this->comments->removeElement($comment)) {
-            // set the owning side to null (unless already changed)
-            if ($comment->getPet() === $this) {
-                $comment->setPet(null);
-            }
-        }
-
-        return $this;
-    }
 }
