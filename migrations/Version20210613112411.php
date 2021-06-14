b47200553b44f912a25ce65f30bc926b9c51b007
moataz-hellal <moatazhellal@insat.u-carthage.tn>
2021-06-14 01:43:12 +0100

    registerForm

---
+++ migrations/Version20210613112411.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210613112411 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE account (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_7D3656A4E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, message LONGTEXT NOT NULL, created_at DATETIME NOT NULL, is_send TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE pet_for_adoption (id INT AUTO_INCREMENT NOT NULL, owner_id INT DEFAULT NULL, kind VARCHAR(255) NOT NULL, age INT DEFAULT NULL, name VARCHAR(255) NOT NULL, gender VARCHAR(255) DEFAULT NULL, location VARCHAR(255) DEFAULT NULL, color VARCHAR(255) DEFAULT NULL, description LONGTEXT DEFAULT NULL, photo_file VARCHAR(255) NOT NULL, INDEX IDX_4CB5C4EA7E3C61F9 (owner_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, a_propos LONGTEXT DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE pet_for_adoption ADD CONSTRAINT FK_4CB5C4EA7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C966F7FB6');
+        $this->addSql('ALTER TABLE blog_post DROP FOREIGN KEY FK_BA5AE01DA76ED395');
+        $this->addSql('ALTER TABLE pet_for_adoption DROP FOREIGN KEY FK_4CB5C4EA7E3C61F9');
+        $this->addSql('DROP TABLE account');
+        $this->addSql('DROP TABLE contact');
+        $this->addSql('DROP TABLE pet_for_adoption');
+        $this->addSql('DROP TABLE user');
+    }
+}
