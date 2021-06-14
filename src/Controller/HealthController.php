091768031211cf0f7abb5826937a420adff4f9f1
HaythemKaouech <haythemkaouech@insat.u-carthage.tn>
2021-06-14 21:51:32 +0100

    health

--- src/Controller/HealthController.php
+++ src/Controller/HealthController.php
@@ -15,4 +15,48 @@ class HealthController extends AbstractController
             'controller_name' => 'HealthController',
         ]);
     }
+
+    #[Route('/skinissues', name: 'skinissues')]
+    public function skinissues(): Response
+    {
+        return $this->render('health/skinissues.html.twig', [
+            'controller_name' => 'HealthController',
+        ]);
+    }
+    #[Route('/dogsick', name: 'dogsick')]
+    public function dogsick(): Response
+    {
+        return $this->render('health/dogsick.html.twig', [
+            'controller_name' => 'HealthController',
+        ]);
+    }
+    #[Route('/catsick', name: 'catsick')]
+    public function catsick(): Response
+    {
+        return $this->render('health/catsick.html.twig', [
+            'controller_name' => 'HealthController',
+        ]);
+    }
+    #[Route('/catfood', name: 'catfood')]
+    public function catfood(): Response
+    {
+        return $this->render('health/catfood.html.twig', [
+            'controller_name' => 'HealthController',
+        ]);
+    }
+    #[Route('/catage', name: 'catage')]
+    public function catage(): Response
+    {
+        return $this->render('health/catage.html.twig', [
+            'controller_name' => 'HealthController',
+        ]);
+    }
+    #[Route('/dogage', name: 'dogage')]
+    public function dogage(): Response
+    {
+        return $this->render('health/dogage.html.twig', [
+            'controller_name' => 'HealthController',
+        ]);
+    }
+
 }
